08ae92ded47a1058b2cfe70a184186e4392d73d5
Gunyez <lagatbrian69@gmail.com>
2026-06-02 11:02:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -36,6 +36,9 @@ app.use("/api/admin", adminRoute);
 app.use("/api/admin/bookings", adminBookingRoute);
 // app.use("/api/mpesa", mpesaRoute);
 
+app.get("/", (req, res) => {
+  res.send("Churrey Backend is running");
+});
 app.get("/test-email", async (req, res) => {
   await sendVerificationEmail("lagatkipkemboi69@gmail.com", "123test");
   res.send("Email test triggered");
