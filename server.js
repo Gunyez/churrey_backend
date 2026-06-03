fb99f516635ceb0217e586f81f7a20582cc6cf6d
Gunyez <lagatbrian69@gmail.com>
2026-06-03 17:13:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(cors({
   origin:[
     "http://localhost:3000",
-    "https://churrey-frontend-xmg5.vercel.app/"
+    "https://churrey-frontend-xmg5.vercel.app"
     ],
   credentials: true
 }));
