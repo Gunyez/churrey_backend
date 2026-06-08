921ef862f5230ae1358899b6f631f2634df6af82
Gunyez <lagatbrian69@gmail.com>
2026-06-08 17:15:28 +0300

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -7,12 +7,14 @@ export const sendVerificationEmail = async (email, token) => {
     }
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      });
 
     await transporter.verify();
 
