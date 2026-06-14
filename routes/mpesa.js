3cd61618ddb633a546e9d8267076c5f82b07e10f
Gunyez <lagatbrian69@gmail.com>
2026-06-14 15:07:22 +0300

    Update mpesa.js

--- routes/mpesa.js
+++ routes/mpesa.js
@@ -4,6 +4,7 @@ import {
   initiatePayment,
   mpesaCallback,
 } from "../controllers/mpesa.js";
+import { getMpesaToken } from "../utils/mpesa.js";
 
 const router = express.Router();
 
@@ -13,7 +14,7 @@ router.post("/callback", mpesaCallback);
 
 router.get("/token", async (req, res) => {
   try {
-    const token = await generateToken();
+    const token = await getMpesaToken();
     res.json({ token });
   } catch (err) {
     console.log(err.response?.data || err);
